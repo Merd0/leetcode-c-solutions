6365b52f96cb6ed140f4acbefe665dde2c1709b3
hp <hp@DESKTOP-L3L5SVF>
2026-06-13 21:30:59 +0300

    Use submitted consistent strings code

--- problems/1684-count-the-number-of-consistent-strings/solution.c
+++ problems/1684-count-the-number-of-consistent-strings/solution.c
@@ -1,12 +1,11 @@
 #include <stdbool.h>
 
-int countConsistentStrings(char* allowed, char** words, int wordsSize)
-{
+int countConsistentStrings(char * allowed, char ** words, int wordsSize) {
     int result = 0;
     int allowedArr[26] = {0};
 
-    for (int i = 0; allowed[i] != '\0'; i++) {
-        allowedArr[allowed[i] - 'a'] = 1;
+    for (int z = 0; allowed[z] != '\0'; z++) {
+        allowedArr[allowed[z] - 'a'] = 1;
     }
 
     for (int i = 0; i < wordsSize; i++) {
